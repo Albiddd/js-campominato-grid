25b1929dba91a206562f6b1aa40f9744ab566a29
Albiddd <alberto.dore93@gmail.com>
2022-07-29 16:42:22 +0200

    js grid generator

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 console.log('Campo Minato')
+
+let dimensioneGriglia = 10;
+let numeroCelle = dimensioneGriglia ** 2;
+const tabelloneEl = document.querySelector('.tabellone');
+
+
+const start = document.querySelector('.button');
+start.addEventListener('click', function(){
+
+for(let i = 0; i < numeroCelle; i++){
+    
+    const cella = getSquareElement();
+    
+    tabelloneEl.append(cella);
+    
+}
+})  
+
+function getSquareElement(){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    
+    square.addEventListener('click', clickHandler); 
+    return square;
+
+}
+
+function clickHandler(){
+    const square = this;
+    square.classList.toggle('clicked')  
+    square.removeEventListener('click', clickHandler)
+}
